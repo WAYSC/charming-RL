b7f3defbc4b3ddd7339b10ac311c8a1941d28e75
Elly <50772845+WAYSC@users.noreply.github.com>
2022-07-23 23:18:01 +0800

    Add files via upload

---
+++ Code/Hands-on Reinforcement Learning/Q-learning/Q_plot.py
@@ -0,0 +1,57 @@
+import numpy as np
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+
+from Q_policy import QLearning
+import os,inspect
+current_dir=os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+os.chdir(current_dir)
+import sys
+sys.path.append('../')
+from Sarsa.env import CliffWalkingEnv
+from Sarsa.print import print_agent
+
+np.random.seed(0)
+epsilon = 0.1
+alpha = 0.1
+gamma = 0.9
+col = 12
+row = 4
+env = CliffWalkingEnv(col, row)
+agent = QLearning(col, row, epsilon, alpha, gamma)
+num_episodes = 500  # 智能体在环境中运行的序列的数量
+
+return_list = []  # 记录每一条序列的回报
+for i in range(10):  # 显示10个进度条
+    # tqdm的进度条功能
+    with tqdm(total=int(num_episodes / 10), desc='Iteration %d' % i) as pbar:
+        for i_episode in range(int(num_episodes / 10)):  # 每个进度条的序列数
+            episode_return = 0
+            state = env.reset()
+            done = False
+            while not done:
+                action = agent.take_action(state)
+                next_state, reward, done = env.step(action)
+                episode_return += reward  # 这里回报的计算不进行折扣因子衰减
+                agent.update(state, action, reward, next_state)
+                state = next_state
+            return_list.append(episode_return)
+            if (i_episode + 1) % 10 == 0:  # 每10条序列打印一下这10条序列的平均回报
+                pbar.set_postfix({
+                    'episode':
+                    '%d' % (num_episodes / 10 * i + i_episode + 1),
+                    'return':
+                    '%.3f' % np.mean(return_list[-10:])
+                })
+            pbar.update(1)
+
+episodes_list = list(range(len(return_list)))
+plt.plot(episodes_list, return_list)
+plt.xlabel('Episodes')
+plt.ylabel('Returns')
+plt.title('Q-learning on {}'.format('Cliff Walking'))
+plt.show()
+
+action_meaning = ['^', 'v', '<', '>']
+print('Q-learning算法最终收敛得到的策略为：')
+print_agent(agent, env, action_meaning, list(range(37, 47)), [47])
